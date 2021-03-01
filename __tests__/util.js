fda19d9cc396a57bb5838255585b99ae6bccdbad
Maksym Shemet <shemet@evergreens.in.ua>
2021-03-01 18:57:05 +0200

    done unit test

--- __tests__/util.js
+++ __tests__/util.js
@@ -1,6 +1,6 @@
 const { generateText } = require('../util');
 
-describe('Unit test example', () => {
+describe('Unit test example:', () => {
   test('should output name and age', () => {
     const text = generateText('Max', 20);
 
