0769d6314d70d93ebafd9c5709bdaa241f72eb91
Maksym Shemet <shemet@evergreens.in.ua>
2021-03-01 18:41:54 +0200

    done unit

---
+++ __tests__/util.js
@@ -0,0 +1,9 @@
+const { generateText } = require('../util');
+
+describe('Unit test example', () => {
+  test('should output name and age', () => {
+    const text = generateText('Max', 20);
+
+    expect(text).toBe('Max (20 years old)');
+  });
+});
