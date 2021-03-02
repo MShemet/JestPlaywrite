f8cefe288830f083040b72692ec4bd0fd81577c5
Maksym Shemet <shemet@evergreens.in.ua>
2021-03-02 13:20:26 +0200

    refactor code

--- util.js
+++ util.js
@@ -1,27 +1,29 @@
 const generateText = (name, age) => {
-  // Returns output text
   return `${name} (${age} years old)`;
 };
 
 const createElement = (type, text, className) => {
-  // Creates a new HTML element and returns it
   const newElement = document.createElement(type);
+
   newElement.classList.add(className);
   newElement.textContent = text;
+
   return newElement;
 };
 
 const validateInput = (text, notEmpty, isNumber) => {
-  // Validate user input with two pre-defined rules
   if (!text) {
     return false;
   }
+
   if (notEmpty && text.trim().length === 0) {
     return false;
   }
+
   if (isNumber && +text === NaN) {
     return false;
   }
+
   return true;
 };
 
@@ -29,6 +31,7 @@ const checkAndGenerate = (name, age) => {
   if (!validateInput(name, true, false) || !validateInput(age, false, true)) {
     return false;
   }
+
   return generateText(name, age);
 };
 
